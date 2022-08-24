fe912a19a1ee0150cb925f7d6a5873de08756ef6
venerari <venerayan@gmail.com>
2022-08-24 17:12:19 -0400

    saving first from fork

--- 01-structuring-routes/src/main/java/org/camelcookbook/structuringroutes/routecontrol/RouteStoppingProcessor.java
+++ 01-structuring-routes/src/main/java/org/camelcookbook/structuringroutes/routecontrol/RouteStoppingProcessor.java
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+// saving testing by venerari
 
 package org.camelcookbook.structuringroutes.routecontrol;
 
